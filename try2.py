e205296dd1d226f0ddf90e2fbadb79823f2c8012
julia <jrdizon4@up.edu.ph>
2018-11-26 18:19:27 +0800

    > added sprites > temporarily moved module codes to try2

--- try2.py
+++ try2.py
@@ -10,9 +10,50 @@ window = pyglet.window.Window(width=500,height=500)
 
 #load assets
 
-img_player = pyglet.image.load('assets/temporary/player.png')
-img_bullet = pyglet.image.load('assets/temporary/bullet.png')
-img_enemy  = pyglet.image.load('assets/temporary/enemy.png')
+img_player = pyglet.image.load('assets/temporary/doge1_50px.png')
+img_bullet = pyglet.image.load('assets/temporary/heart.png')
+img_enemy  = pyglet.image.load('assets/temporary/chocolate_28px.png')
+
+
+#create new player
+def new_player():
+	a = { "sprite" : pyglet.sprite.Sprite(img_player) }
+	a["sprite"].position = (10,0)
+	#define update function
+	def update(dt):
+		spr = a["sprite"]
+
+		#follow mouse
+		spr.position = ( 10 , spr.position[1] + (mouse_position[1] - spr.position[1])//10 )
+
+		#deform sprite
+		spr.scale_y  = 1 - (mouse_position[1] - spr.position[1])/500;
+
+	#save the function indside the dictinory
+	a["update"] = update
+	return a
+
+
+#create enemy
+def new_enemey():
+	# define
+	a = {
+			"sprite":  pyglet.sprite.Sprite(img_enemy) ,
+			"speed" :  200,
+		}
+	#ilagay yung enemy sa right ng screen tapos random  yung height
+	a["sprite"].position = ( 500 , 500*random.uniform(-1,1)  )
+	#update values every frame
+	def update(dt):
+		spr = a["sprite"]
+		#move papuntang left
+		spr.position = (spr.position[0] - a["speed"]*dt  , spr.position[1])
+	#save the function indside the dictinory
+	a["update"]  = update
+	#add to the list of Enemies
+	Enemies.append(a);
+	#return the new enemy object
+	return a;
 
 
 #create_projectile
@@ -43,10 +84,6 @@ Projectile = []
 
 #global vars
 mouse_position = [0,0]
-
-
-
-
 
 
 def update(dt):
