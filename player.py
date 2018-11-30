25cd48cbbf61d54df99651480e6a01cbe91b1c8c
Renzo Virtucio <renzojohnvirtucio@gmail.com>
2018-11-30 20:21:23 +0800

    player sprite follows mouse position and changes in main file

---
+++ player.py
@@ -0,0 +1,31 @@
+import pyglet
+import random
+import math
+
+
+def setImg(new_img):
+	global img
+	img = new_img
+
+def add(mouse_position):
+	global a
+	a = {
+			"sprite":  pyglet.sprite.Sprite(img)
+		}
+	
+	a["sprite"].position = (0, 0)
+	def update(dt):
+		spr = a["sprite"]
+		spr.position = (25, mouse_position[1] - 25)
+	
+	a["update"] = update
+	
+	return a
+
+def update(dt):
+	a["update"](dt)
+
+
+def draw():
+	a["sprite"].draw()
+
