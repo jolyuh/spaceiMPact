8ba88c6a61fa2e9bcc1c434184a789f1bf113f25
julia <jrdizon4@up.edu.ph>
2018-11-30 17:48:51 +0800

    minor edits

--- enemy.py
+++ enemy.py
@@ -7,44 +7,44 @@ img = [None, None, None]
 
 
 def setImg(new_img):
-	global img
-	img = new_img
+    global img
+    img = new_img
 
 
 def add(enemy_type):
-	a = {
-			"sprite":  pyglet.sprite.Sprite(img[enemy_type]),
-			"speed":  100,
-			"omega": 0.2*random.uniform(-1, 1),
-			"initial_y": 720*random.uniform(-1, 1)
-		}
+    a = {
+            "sprite":  pyglet.sprite.Sprite(img[enemy_type]),
+            "speed":  100,
+            "omega": 0.2*random.uniform(-1, 1),
+            "initial_y": 720*random.uniform(-1, 1)
+        }
 
-	a["sprite"].position = (720, a["initial_y"])
-	a["sprite"].anchor = (12, 14)
+    a["sprite"].position = (720, a["initial_y"])
+    a["sprite"].anchor = (12, 14)
 
-	def update1(dt):
-		spr = a["sprite"]
-		spr.rotation += a["omega"]
-		spr.position = (spr.position[0] - a["speed"]*dt, a["initial_y"])
+    def update1(dt):
+        spr = a["sprite"]
+        spr.rotation += a["omega"]
+        spr.position = (spr.position[0] - a["speed"]*dt, a["initial_y"])
+    
+    def update2(dt):
+        spr = a["sprite"]
+        spr.position = (spr.position[0] - a["speed"]*dt, a["initial_y"] + 100*math.sin(spr.position[0]/60))
 
-	def update2(dt):
-		spr = a["sprite"]
-		spr.position = (spr.position[0] - a["speed"]*dt, a["initial_y"] + 100*math.sin(spr.position[0]/60))
+    def update3(dt):
+        spr = a["sprite"]
+        spr.position = (spr.position[0] - a["speed"]*dt, a["initial_y"] + math.sin(spr.position[0]))
 
-	def update3(dt):
-		spr = a["sprite"]
-		spr.position = (spr.position[0] - a["speed"]*dt, a["initial_y"] + math.sin(spr.position[0]))
-
-	a["update"] = [update1, update2, update3][enemy_type]
-	Enemies.append(a)
-	return a
+    a["update"] = [update1, update2, update3][enemy_type]
+    Enemies.append(a)
+    return a
 
 
 def update(dt):
-	for i in Enemies:
-		i["update"](dt)
+    for i in Enemies:
+        i["update"](dt)
 
 
 def draw():
-	for i in Enemies:
-		i["sprite"].draw()
+    for i in Enemies:
+        i["sprite"].draw()
