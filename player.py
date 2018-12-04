937c2a620b267d27ca9232d3678c9379aadf50a4
Julia Francesca Dizon <44121122+jrdizon4@users.noreply.github.com>
2018-12-04 16:09:34 +0800

    changed variable "a" in player to "pl" for clarity

--- player.py
+++ player.py
@@ -8,43 +8,43 @@ def set_img(new_img):
 
 
 def add(mouse_position):
-    global a
-    a = {
+    global pl
+    pl = {
             "sprite":  pyglet.sprite.Sprite(img),
             "immune": False
         }
 
-    a["sprite"].scale = 0.2381
-    a["sprite"].position = (0, 0)
+    pl["sprite"].scale = 0.2381
+    pl["sprite"].position = (0, 0)
     global counter
     counter = 0
 
     def update_player(dt):
         global counter
-        spr = a["sprite"]
+        spr = pl["sprite"]
 
         target = (25 + 200 * ((mouse_position[0] - 100)/720), mouse_position[1] - 25)
         spr.position = (spr.position[0] + (target[0]-spr.position[0])/4, spr.position[1] + (target[1]-spr.position[1])/4)
         
-        if counter > 2*60 and a["immune"] is True:
-            a["immune"] = False
+        if counter > 2*60 and pl["immune"] is True:
+            pl["immune"] = False
             counter = 0
 
-        if a["immune"]:
-            a["sprite"].color = (100+math.floor(77+77*math.sin(counter)), 100, 100)
+        if pl["immune"]:
+            pl["sprite"].color = (100+math.floor(77+77*math.sin(counter)), 100, 100)
         else:
-            a["sprite"].color = (255, 255, 255)
+            pl["sprite"].color = (255, 255, 255)
             
-    a["update"] = update_player
+    pl["update"] = update_player
 
-    return a
+    return pl
 
 
 def update(dt):
     global counter
     counter += 1
-    a["update"](dt)
+    pl["update"](dt)
 
 
 def draw():
-    a["sprite"].draw()
+    pl["sprite"].draw()
