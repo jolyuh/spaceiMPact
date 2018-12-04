5ece765d19e5ef086fb229908084b3aa7797972d
Julia Francesca Dizon <44121122+jrdizon4@users.noreply.github.com>
2018-12-04 16:40:04 +0800

    changed variables "a" and "i" to "e"

--- enemy.py
+++ enemy.py
@@ -12,7 +12,7 @@ def set_img(new_img):
 
 
 def add(enemy_type):
-    a = {
+    e = {
             "sprite":  pyglet.sprite.Sprite(img[enemy_type]),
             "speed":  100,
             "omega": 0.2*random.uniform(-1, 1),
@@ -22,26 +22,26 @@ def add(enemy_type):
         }
 
     if enemy_type == 2:
-        a["time"] = 0
-        a["sprite"].position = (720, 240)
-        a["target"] = (500, 220)
-        a["boss"] = True
-        a["lives"] = 25
+        e["time"] = 0
+        e["sprite"].position = (720, 240)
+        e["target"] = (500, 220)
+        e["boss"] = True
+        e["lives"] = 25
     else:
-        a["sprite"].position = (720, a["initial_y"])
-    a["sprite"].anchor = (12, 14)
+        e["sprite"].position = (720, e["initial_y"])
+    e["sprite"].anchor = (12, 14)
 
     def update1(dt, player):
-        spr = a["sprite"]
-        spr.rotation += a["omega"]
-        spr.position = (spr.position[0] - a["speed"]*dt, a["initial_y"])
+        spr = e["sprite"]
+        spr.rotation += e["omega"]
+        spr.position = (spr.position[0] - e["speed"]*dt, e["initial_y"])
     
     def update2(dt, player):
-        spr = a["sprite"]
-        spr.position = (spr.position[0] - a["speed"]*dt, a["initial_y"] + 100*math.sin(spr.position[0]/60))
+        spr = e["sprite"]
+        spr.position = (spr.position[0] - e["speed"]*dt, e["initial_y"] + 100*math.sin(spr.position[0]/60))
 
     def goto(pos):
-        spr = a["sprite"]
+        spr = e["sprite"]
 
         dx = pos[0] - spr.position[0]
         dy = pos[1] - spr.position[1]
@@ -49,53 +49,53 @@ def add(enemy_type):
         spr.position = (spr.position[0] + dx/20, spr.position[1] + dy/20)
 
     def update3(dt, player):
-        spr = a["sprite"]
-        t = a["time"]
+        spr = e["sprite"]
+        t = e["time"]
 
         if t < 30:
             goto((500, 220))
         else:
             m = t % (3*60)
             if m == 0:
-                a["target"] = (550, 220)
+                e["target"] = (550, 220)
             elif m == 30:
-                a["target"] = (450, 220 + 20*random.uniform(-1, 1))
+                e["target"] = (450, 220 + 20*random.uniform(-1, 1))
             elif m == 40:
-                a["target"] = (550, 220 + 20*random.uniform(-1, 1))
+                e["target"] = (550, 220 + 20*random.uniform(-1, 1))
             elif m == 60:
                 if random.uniform(0, 1) > 0.2:
-                    a["target"] = player["sprite"].position 
+                    e["target"] = player["sprite"].position 
                 else:
-                    a["target"] = (450, 220)
+                    e["target"] = (450, 220)
             elif m == 90:
                 if t > 60*60:
                     if random.uniform(0, 1) > 0.5:
-                        a["target"] = (player["sprite"].position[0] + 100*random.uniform(-1, 1), player["sprite"].position[1])
+                        e["target"] = (player["sprite"].position[0] + 100*random.uniform(-1, 1), player["sprite"].position[1])
                 else:
                     if random.uniform(0, 1) > 0.8:
-                        a["target"] = player["sprite"].position
+                        e["target"] = player["sprite"].position
             elif m == 150:
-                a["target"] = (550, 220)
+                e["target"] = (550, 220)
 
             if t > 60*60:
-                a["sprite"].rotation = 2*random.uniform(-1, 1)
+                e["sprite"].rotation = 2*random.uniform(-1, 1)
 
-            a["sprite"].color = (255, (255-a["sprite"].color[1])/3 + a["sprite"].color[1], (255-a["sprite"].color[2])/3 + a["sprite"].color[2])
+            e["sprite"].color = (255, (255-e["sprite"].color[1])/3 + e["sprite"].color[1], (255-e["sprite"].color[2])/3 + e["sprite"].color[2])
 
-            goto(a["target"])
+            goto(e["target"])
 
-        a["time"] += 1
+        e["time"] += 1
 
-    a["update"] = [update1, update2, update3][enemy_type]
-    Enemies.append(a)
-    return a
+    e["update"] = [update1, update2, update3][enemy_type]
+    Enemies.append(e)
+    return e
 
 
 def update(dt, player):
-    for i in Enemies:
-        i["update"](dt, player)
+    for e in Enemies:
+        e["update"](dt, player)
 
 
 def draw():
-    for i in Enemies:
-        i["sprite"].draw()
+    for e in Enemies:
+        e["sprite"].draw()
